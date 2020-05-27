b56fc385964b51deeb0322c626b1910cf35d454d
zhangbingjian <zbj19905984421@163.com>
2020-05-27 18:55:22 +0800

    feat:  完善button样式,合并组件里props,按钮属性和a标签属性

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,39 @@ import Button, {ButtonType, ButtonSize} from './components/Button/button';
 function App() {
     return (
         <>
-            <Button></Button>
+            <span> </span>
+            <Button
+                onClick={() => {
+                    alert('123');
+                }}></Button>
+            <span> </span>
             <Button btnType={ButtonType.Promary} size={ButtonSize.Large}></Button>
-            <Button href="http://www.baidu.com" btnType={ButtonType.Link} disabled></Button>
+            <span> </span>
+            <Button btnType={ButtonType.Link} disabled></Button>
+            <div>
+                <Button btnType={ButtonType.Link}></Button>
+            </div>
+            <span> </span>
             <Button btnType={ButtonType.Promary} size={ButtonSize.Large} disabled></Button>
+            <span> </span>
+            <Button btnType={ButtonType.Promary} size={ButtonSize.Small} disabled></Button>
+            <span> </span>
+            <Button btnType={ButtonType.Danger} size={ButtonSize.Small}></Button>
+            <span> </span>
+
+            <Button size={ButtonSize.Custom} width="300px" height="50px"></Button>
+            <span> </span>
+
+            <Button
+                onClick={() => {
+                    alert('a');
+                }}
+                btnType={ButtonType.Link}
+                size={ButtonSize.Custom}
+                width="300px"
+                height="50px"
+                href="http://www.baidu.com"></Button>
+            <span> </span>
         </>
     );
 }
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,13 +1,20 @@
 /** @format */
 
+/**
+ * @fileOverview button按钮组件
+ * @author: 炳健
+ * @time: 2020-5-27
+ */
+
 import React from 'react';
 import classNames from 'classnames';
 
-export enum ButtonSize {
+export enum ButtonSize { //按钮的大小
     Large = 'lg',
     Small = 'sm',
+    Custom = 'custom',
 }
-export enum ButtonType {
+export enum ButtonType { //按钮类型
     Promary = 'primary',
     Default = 'default',
     Danger = 'danger',
@@ -21,26 +28,59 @@ interface BaseButtonProps {
     btnType?: ButtonType;
     children?: React.ReactNode;
     href?: string;
+    width?: string;
+    height?: string;
 }
 
-const Button: React.FC<BaseButtonProps> = props => {
-    const {btnType, disabled, size, children, href} = props;
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
-    const classes = classNames('btn', {
+const Button: React.FC<ButtonProps> = props => {
+    const {btnType, disabled, size, children, href, width, height, className, ...restProps} = props;
+
+    const classes = classNames('btn', className, {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
         disabled: btnType === ButtonType.Link && disabled,
     });
 
+    if (size === ButtonSize.Custom) {
+        if (btnType === ButtonType.Link && href) {
+            return (
+                <a
+                    href={href}
+                    className={classes}
+                    {...restProps}
+                    style={{
+                        width,
+                        height,
+                        display: 'flex',
+                        textAlign: 'center',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                    {children}
+                </a>
+            );
+        } else {
+            return (
+                <button className={classes} {...restProps} style={{width, height}} disabled={disabled}>
+                    {children}
+                </button>
+            );
+        }
+    }
+
     if (btnType === ButtonType.Link && href) {
         return (
-            <a href={href} className={classes}>
+            <a href={href} className={classes} {...restProps}>
                 {children}
             </a>
         );
     } else {
         return (
-            <button className={classes} disabled={disabled}>
+            <button className={classes} disabled={disabled} {...restProps}>
                 {children}
             </button>
         );

3a63ef14ffb37de0842bb224df0099447f1acbea
zhangbingjian <zbj19905984421@163.com>
2020-05-28 18:44:32 +0800

    fix: 将alert  close按钮增大

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,14 @@ import Alert, {AlertType} from './components/Alert/alert';
 function App() {
     return (
         <div style={{width: '300px', margin: '100px'}}>
-            <Alert type={AlertType.Info} message="123333" closable />
+            <Alert
+                type={AlertType.Info}
+                message="123333"
+                closable
+                onClose={() => {
+                    console.log(123);
+                }}
+            />
             <hr />
             <Alert type={AlertType.Success} closable>
                 <span>1231231231231内容</span>

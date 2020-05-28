b48e8c3912323283516c363124f74effb66d169a
zhangbingjian <zbj19905984421@163.com>
2020-05-28 14:14:47 +0800

    feat : 添加按钮形状与与父元素同宽

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 /** @format */
 import React from 'react';
-import Button, {ButtonType, ButtonSize} from './components/Button/button';
+import Button, {ButtonType, ButtonSize, ButtonShape} from './components/Button/button';
 
 function App() {
     return (
-        <>
+        <div style={{paddingLeft: '300px'}}>
             <span> </span>
             <Button
                 onClick={() => {
                     alert('123');
-                }}></Button>
+                }}
+                width="200px">
+                333
+            </Button>
             <span> </span>
             <Button btnType={ButtonType.Promary} size={ButtonSize.Large}></Button>
             <span> </span>
             <Button btnType={ButtonType.Link} disabled></Button>
-            <div>
-                <Button btnType={ButtonType.Link}></Button>
-            </div>
             <span> </span>
             <Button btnType={ButtonType.Promary} size={ButtonSize.Large} disabled></Button>
             <span> </span>
@@ -24,21 +24,37 @@ function App() {
             <span> </span>
             <Button btnType={ButtonType.Danger} size={ButtonSize.Small}></Button>
             <span> </span>
-
             <Button size={ButtonSize.Custom} width="300px" height="50px"></Button>
             <span> </span>
-
             <Button
                 onClick={() => {
                     alert('a');
                 }}
                 btnType={ButtonType.Link}
-                size={ButtonSize.Custom}
                 width="300px"
                 height="50px"
                 href="http://www.baidu.com"></Button>
             <span> </span>
-        </>
+            <p>12</p>
+            <Button btnType={ButtonType.Promary}>099</Button>
+            <hr />
+            <hr />
+            <hr />
+            <div style={{width: '500px', backgroundColor: '#ccc', height: '200px'}}>
+                <Button btnType={ButtonType.Danger} block>
+                    333
+                </Button>
+                <span>123</span>
+            </div>
+            <hr />
+            <Button size={ButtonSize.Small} btnType={ButtonType.Danger} shape={ButtonShape.Circle}></Button>
+            <Button
+                size={ButtonSize.Large}
+                btnType={ButtonType.Link}
+                width={'300px'}
+                height={'90px'}
+                shape={ButtonShape.Circle}></Button>
+        </div>
     );
 }
 
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -14,6 +14,10 @@ export enum ButtonSize { //按钮的大小
     Small = 'sm',
     Custom = 'custom',
 }
+export enum ButtonShape { //按钮类型
+    Circle = 'circle',
+}
+
 export enum ButtonType { //按钮类型
     Promary = 'primary',
     Default = 'default',
@@ -23,13 +27,15 @@ export enum ButtonType { //按钮类型
 
 interface BaseButtonProps {
     className?: string;
-    disabled?: boolean;
-    size?: ButtonSize;
-    btnType?: ButtonType;
-    children?: React.ReactNode;
-    href?: string;
-    width?: string;
-    height?: string;
+    disabled?: boolean; //禁用状态
+    size?: ButtonSize; //按钮大小
+    btnType?: ButtonType; //按钮的类型
+    children?: React.ReactNode; //按钮内部元素
+    href?: string; //按钮链接使用 (btnType为Link时必传)
+    width?: string; // 按钮宽度(size为custom时有效)
+    height?: string; // 按钮高度(size为custom时有效)
+    shape?: ButtonShape; //按钮形状 不传为小圆角
+    block?: Boolean; // 将按钮宽度调整为其父宽度
 }
 
 type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>;
@@ -37,35 +43,41 @@ type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElemen
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
 const Button: React.FC<ButtonProps> = props => {
-    const {btnType, disabled, size, children, href, width, height, className, ...restProps} = props;
+    const {btnType, disabled, size, children, href, width, height, className, shape, block, ...restProps} = props;
 
     const classes = classNames('btn', className, {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
+        'btn-block': block,
+        [`btn-shape-${shape}`]: shape,
         disabled: btnType === ButtonType.Link && disabled,
     });
 
     if (size === ButtonSize.Custom) {
         if (btnType === ButtonType.Link && href) {
             return (
-                <a
-                    href={href}
-                    className={classes}
-                    {...restProps}
-                    style={{
-                        width,
-                        height,
-                        display: 'flex',
-                        textAlign: 'center',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
-                    {children}
-                </a>
+                <div style={{width, height, display: 'inline-block'}}>
+                    <a
+                        href={href}
+                        className={classes}
+                        {...restProps}
+                        style={{
+                            padding: 0,
+                            display: 'block',
+                            height: '100%',
+                            lineHeight: height,
+                        }}>
+                        {children}
+                    </a>
+                </div>
             );
         } else {
             return (
-                <button className={classes} {...restProps} style={{width, height}} disabled={disabled}>
+                <button
+                    className={classes}
+                    {...restProps}
+                    style={{width: block ? '100%' : width, height}}
+                    disabled={disabled}>
                     {children}
                 </button>
             );
